7367faccf7faa89298ae52ac91dace3ec7a42aec
kazmathui <46832172+kaz-mathui@users.noreply.github.com>
2024-03-03 19:44:15 +0900

    apply touchableopacity

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,4 +1,10 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
 
 import Header from '../../components/Header'
 import Button from '../../components/Button'
@@ -11,10 +17,12 @@ const SignUp = (): JSX.Element => {
         <Text style={styles.title}>Sign Up</Text>
         <TextInput style={styles.input} value='Email address' />
         <TextInput style={styles.input} value='Password' />
-        <Button label='label' />
+        <Button label='Submit' />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already resistered?</Text>
-          <Text style={styles.footerLink}>Log in.</Text>
+          <TouchableOpacity>
+            <Text style={styles.footerLink}>Log in.</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
